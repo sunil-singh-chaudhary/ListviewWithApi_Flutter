2188ac466934b66862329f8837935a10af82e9c6
Sunil Chaudhary <chaudharysunil62@gmail.com>
2022-07-22 18:56:47 +0530

    getting json from assets

--- lib/Catalog_model.dart
+++ lib/Catalog_model.dart
@@ -1,23 +1,21 @@
 class Catalog_model {
   static final items = [
     Item(
-        id: 1,
+       
         name: "iPhone 12 Pro",
         desc: "Apple iPhone 12th generation",
-        price: 999,
-        color: "#33505a",
+      
         image:
             "https://www.geekrar.com/wp-content/uploads/2020/11/iphone-13-pro-render-1024x576_1605254769-1140x570-1-770x515.jpg")
   ];
 }
 
 class Item {
-  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
+  Item({ this.name, this.desc, this.image});
 
-  final String? color;
+
   final String? desc;
-  final int? id;
   final String? image;
   final String? name;
-  final num? price;
+
 }
--- lib/api_handler_class.dart
+++ lib/api_handler_class.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:api_listview_flutte_demo/Catalog_model.dart';
 import 'package:api_listview_flutte_demo/itemWidget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -12,17 +15,32 @@ class api_handler_class extends StatefulWidget {
 }
 
 class _api_handler_classState extends State<api_handler_class> {
-  final dummy_list = List.generate(20, (index) => Catalog_model.items[0]);
+  @override
+  void initState() {
+    readJson();
+
+    super.initState();
+  }
+
+  Future<void> readJson() async {
+    final String response =
+        await rootBundle.loadString('assets/files/api_data.json');
+    final data = await jsonDecode(response);
+    var productData = data["Products"];
+    print(productData);
+  }
+
+  //final dummy_list = List.generate(20, (index) => Catalog_model.items[0]);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Items Listview")),
+      appBar: AppBar(title: const Text("Items Listview")),
       body: Padding(
-          padding: EdgeInsets.all(10),
+          padding: const EdgeInsets.all(10),
           child: ListView.builder(
-            itemCount: dummy_list.length,
+            itemCount: Catalog_model.items.length,
             itemBuilder: (context, index) =>
-                itemWidget(item: dummy_list[index]),
+                itemWidget(item: Catalog_model.items[index]),
           )),
     );
   }
